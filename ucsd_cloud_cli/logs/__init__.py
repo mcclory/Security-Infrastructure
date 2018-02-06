6d2d6e1c6c2e626a124ab23a4c10e2603daa9f89
Patrick McClory <pmdev@introspectdata.com>
2018-02-06 14:58:28 -0800

    adding notes for implementation around s3 bucket and log aggregation from other accounts.

--- ucsd_cloud_cli/logs/__init__.py
+++ ucsd_cloud_cli/logs/__init__.py
@@ -2,5 +2,6 @@ import click
 from .cloudtrail import cloudtrail
 from .cloudwatch import cloudwatch
 from .vpc import vpc
+from .. import cf_data_dir
 
 logs = click.CommandCollection(sources=[cloudtrail, cloudwatch, vpc])
