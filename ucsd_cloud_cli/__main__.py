866ad2b086bef994bfde9224047c64ad5a52b71e
Patrick McClory <pmdev@introspectdata.com>
2018-02-08 12:57:51 -0800

    validating that we are outputting a yaml template, will work on shuffling this around to get to a viable deployment test

--- ucsd_cloud_cli/__main__.py
+++ ucsd_cloud_cli/__main__.py
@@ -2,7 +2,7 @@ import click
 from .logs import logs
 from .sec import sec
 
-cli = click.CommandCollection(sources=[sec, logs])
+cli = click.CommandCollection(sources=[ logs])
 
 if __name__ == '__main__':
     cli()
--- ucsd_cloud_cli/logs/__init__.py
+++ ucsd_cloud_cli/logs/__init__.py
@@ -1,7 +1,6 @@
 import click
-from .cloudtrail import cloudtrail
-from .cloudwatch import cloudwatch
-from .vpc import vpc
+from .target import target
 from .. import cf_data_dir
 
-logs = click.CommandCollection(sources=[cloudtrail, cloudwatch, vpc])
+
+logs = click.CommandCollection(sources=[target])
