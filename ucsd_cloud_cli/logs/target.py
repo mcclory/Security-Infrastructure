47db8358cc13bf65cc1fc880e20d803cf52e05af
Patrick McClory <pmdev@introspectdata.com>
2018-02-10 07:14:53 -0800

    updates to include docuemntation at a command level and prompt fixes to generate method that are dependent on environment variable for ease of use

--- ucsd_cloud_cli/logs/target.py
+++ ucsd_cloud_cli/logs/target.py
@@ -60,12 +60,21 @@ def initialize(account_id_list, bucket_name=None, log_file_prefix=None, aws_prof
 
 
 @target.command()
-@click.option('--dry-run', 'dry_run', is_flag=True)
-@click.option('--deploy-account-id', '-d', 'deploy_account_id')
-@click.option('--deploy-region-name', '-n', 'deploy_region_name')
-@click.option('-r', '--region', 'region_list', multiple=True)
-@click.option('-a', '--account', 'account_list', multiple=True)
+@click.option('--deploy-account-id', '-d', 'deploy_account_id', prompt='Deploy Account ID' if os.getenv('CLI_PROMPT') else None)
+@click.option('--deploy-region-name', '-n', 'deploy_region_name', prompt='Deploy Region Name' if os.getenv('CLI_PROMPT') else None)
+@click.option('-r', '--region', 'region_list', multiple=True, prompt='Child Account Region List' if os.getenv('CLI_PROMPT') else None)
+@click.option('-a', '--account', 'account_list', multiple=True, prompt='Child Account ID List' if os.getenv('CLI_PROMPT') else None)
+@click.option('--dry-run', 'dry_run', is_flag=True, prompt='Dry Run' if os.getenv('CLI_PROMPT') else None)
 def generate(deploy_account_id='123456789012', deploy_region_name='us-west-2', account_list=None, region_list=None, dry_run=False):
+    """CloudFormation template generator for use in creating the resources required to capture logs in a centrally managed account per UCSD standards.
+
+    Keyword Arguments:
+    deploy_account_id -- 'this' account id (where template is being installed) - this is an artifact of how log subscription policies need to be set
+    deploy_region_name -- 'this' region (where template is installed) - this is an artifact of how log subscription policies need to be set
+    account_list -- list of accounts that are to be allowed to publish logs into 'this' account (where the template is installed)
+    region_list -- list of regions that are being used to deploy into - affects policies created to allow cross-account communciation
+    dry_run -- boolean indicates whether template should be printed to screen vs. being saved to file
+    """
     if type(account_list) == tuple:
         account_list = list(account_list)
 
