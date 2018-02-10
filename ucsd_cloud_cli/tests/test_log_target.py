282071f43c79d16480b6fb0e0051c62bcbc11e97
Patrick McClory <pmdev@introspectdata.com>
2018-02-10 10:38:17 -0800

    adding cloudformation parse tests

--- ucsd_cloud_cli/tests/test_log_target.py
+++ ucsd_cloud_cli/tests/test_log_target.py
@@ -4,6 +4,7 @@ import unittest
 import json
 from click.testing import CliRunner
 from ucsd_cloud_cli import cli
+from .cfn2py import validate_doc
 
 class TestLogTarget(unittest.TestCase):
 
@@ -19,3 +20,7 @@ class TestLogTarget(unittest.TestCase):
         result = self.runner.invoke(cli, ['target', 'generate', '--help'])
         for arg_name in ['-d', '--deploy-account-id', '-n', '--deploy-region-name', '-r', '--region', '-a', '--account', '--dry-run']:
             assert arg_name in result.output
+
+    def test_cfn_structure(self):
+        result = self.runner.invoke(cli, ['target', 'generate', '--dry-run'])
+        assert validate_doc(result.output)
