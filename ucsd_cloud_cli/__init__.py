c5d39fa8c0587146cbce6fb255a17a227b1491ef
Patrick McClory <pmdev@introspectdata.com>
2018-02-12 18:50:11 -0800

    fixing version structure and adding python 3.6 to the classifiers

--- ucsd_cloud_cli/__init__.py
+++ ucsd_cloud_cli/__init__.py
@@ -8,3 +8,5 @@ data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 cf_data_dir = os.path.join(data_dir, 'cloudformation')
 
 cli = click.CommandCollection(sources=[logs])
+
+VERSION = '0.1.0'
